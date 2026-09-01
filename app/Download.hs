cd8293610bb1d1261490195c664f0d05ac7b449d
a <a@as-Virtual-Machine.local>
2026-09-01 15:25:14 -0700

    Download manifest.json using wget in the download script

--- app/Download.hs
+++ app/Download.hs
@@ -3,12 +3,17 @@ module Download where
 import Relude
 import System.Directory (createDirectoryIfMissing, getHomeDirectory)
 import System.FilePath ((</>))
+import System.Process (callProcess)
 
 main :: IO ()
 main = do
   home <- getHomeDirectory
   let statePath = home </> ".local/state/sense"
       partsPath = statePath </> "parts"
+      bar = statePath </> "manifest.json"
       extractedPath = statePath </> "raw-wiktextract-data.jsonl"
   createDirectoryIfMissing True partsPath
-  pure ()
+  callProcess "wget" ["-O", bar, foo]
+
+foo :: String
+foo = "https://raw.githubusercontent.com/8ta4/mean-data/0a69fe730a0ea1bfaef84eba0dbe0f68ce991683/manifest.json"
