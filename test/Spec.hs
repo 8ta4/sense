6ba74b972c3c4a7e4a3dfed683769dffa5435316
a <a@as-Virtual-Machine.local>
2026-09-02 00:59:44 -0700

    Print the generated request payload in the test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,12 @@
 module Spec where
 
 import Relude
-import Sense (loadApiKeyHeader)
+import Sense (loadApiKeyHeader, makeRequestPayload)
 
 main :: IO ()
 main = do
   apiKeyHeader <- loadApiKeyHeader
+  let payload = makeRequestPayload "fat" "sense" "Any of the manners by which living beings perceive the physical world: for humans sight, smell, hearing, touch, taste."
+  putTextLn "Payload:"
+  print payload
   pure ()
