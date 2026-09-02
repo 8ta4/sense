8f32f7e894f6f02336386de0c98fb92d4722116f
a <a@as-Virtual-Machine.local>
2026-09-02 00:51:52 -0700

    Implement makeRequestPayload to build the API request body

--- app/Sense.hs
+++ app/Sense.hs
@@ -1,6 +1,7 @@
 module Sense where
 
-import Data.Aeson (ToJSON, encode)
+import Data.Aeson (KeyValue ((.=)), ToJSON, Value, encode, object)
+import Data.Aeson.Key
 import Network.HTTP.Req (Option, Scheme (Https), header)
 import Relude
 import System.Directory (getHomeDirectory)
@@ -15,6 +16,52 @@ loadApiKeyHeader = do
   apiKey <- readFileBS $ home </> ".config/sense/key"
   pure $ header "x-goog-api-key" apiKey
 
+makeRequestPayload :: Text -> Text -> Text -> Value
+makeRequestPayload targetPhrase targetMeaning targetTopic =
+  object
+    [ "contents"
+        .= [ object
+               [ "parts"
+                   .= [ object
+                          ["text" .= (renderEdn benchmarkPhrase benchmarkMeaning benchmarkTopic <> "\n" <> renderEdn targetPhrase targetMeaning targetTopic)]
+                      ]
+               ]
+           ],
+      "generation_config"
+        .= object
+          [ "max_output_tokens" .= (2 :: Int) ^ (7 :: Int),
+            "response_mime_type" .= ("application/json" :: Text),
+            -- Using camelCase (`responseJsonSchema`) causes the Gemini Batch API to generate incorrect properties in the output.
+            -- To ensure the schema is applied correctly, we use snake_case (`response_json_schema`).
+            "response_json_schema"
+              .= object
+                [ "additional_properties" .= False,
+                  "properties"
+                    .= object
+                      [ fromText benchmarkPhrase
+                          .= percentageSchema,
+                        fromText targetPhrase
+                          .= percentageSchema
+                      ],
+                  "property_ordering" .= [fromText benchmarkPhrase, fromText targetPhrase],
+                  "required" .= [fromText benchmarkPhrase, fromText targetPhrase],
+                  "type" .= ("object" :: Text)
+                ],
+            "seed" .= (0 :: Int),
+            "temperature" .= (0 :: Int),
+            "thinking_config"
+              .= object
+                ["thinking_level" .= ("MINIMAL" :: Text)]
+          ],
+      "system_instruction"
+        .= object
+          [ "parts"
+              .= [ object
+                     ["text" .= systemPrompt]
+                 ]
+          ]
+    ]
+
 -- Haskell's `show` escapes non-ASCII Unicode characters using decimal escape sequences.
 -- `renderJson` uses JSON string escaping.
 renderEdn :: Text -> Text -> Text -> Text
@@ -22,3 +69,23 @@ renderEdn phrase meaning topic = "{:phrase " <> renderJson phrase <> " :meaning 
 
 renderJson :: (ToJSON a) => a -> Text
 renderJson = decodeUtf8 <$> encode
+
+benchmarkPhrase :: Text
+benchmarkPhrase = "dog"
+
+benchmarkMeaning :: Text
+benchmarkMeaning = "A dull, unattractive girl or woman."
+
+benchmarkTopic :: Text
+benchmarkTopic = "ugly"
+
+percentageSchema :: Value
+percentageSchema =
+  object
+    [ "maximum" .= (100 :: Int),
+      "minimum" .= (0 :: Int),
+      "type" .= ("number" :: Text)
+    ]
+
+systemPrompt :: Text
+systemPrompt = "Estimate the percentage of Americans 10 years or older who consider each meaning on topic."
