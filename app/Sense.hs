3b15a4dc51cee5d968b3760a3d8f8d106dc5225f
a <a@as-Virtual-Machine.local>
2026-09-02 00:43:08 -0700

    Add renderEdn function to format phrases, meanings, and topics into EDN maps

--- app/Sense.hs
+++ app/Sense.hs
@@ -1,5 +1,6 @@
 module Sense where
 
+import Data.Aeson (ToJSON, encode)
 import Network.HTTP.Req (Option, Scheme (Https), header)
 import Relude
 import System.Directory (getHomeDirectory)
@@ -13,3 +14,11 @@ loadApiKeyHeader = do
   home <- getHomeDirectory
   apiKey <- readFileBS $ home </> ".config/sense/key"
   pure $ header "x-goog-api-key" apiKey
+
+-- Haskell's `show` escapes non-ASCII Unicode characters using decimal escape sequences.
+-- `renderJson` uses JSON string escaping.
+renderEdn :: Text -> Text -> Text -> Text
+renderEdn phrase meaning topic = "{:phrase " <> renderJson phrase <> " :meaning " <> renderJson meaning <> " :topic " <> renderJson topic <> "}"
+
+renderJson :: (ToJSON a) => a -> Text
+renderJson = decodeUtf8 <$> encode
