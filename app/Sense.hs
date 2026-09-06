eb9c0b614c6595fada36b24706dcc6255c61a978
a <a@as-Virtual-Machine.local>
2026-09-06 01:22:42 -0700

    Remove duplicate entries using ordNub

--- app/Sense.hs
+++ app/Sense.hs
@@ -53,7 +53,7 @@ main = do
                   )
           ensureSubmitted = do
             content <- readFileLBS wiktextractPath
-            let _ = (filter isTarget $ mapMaybe decode $ Char8.lines content) >>= processEntry
+            let _ = ordNub ((filter isTarget $ mapMaybe decode $ Char8.lines content) >>= processEntry)
             pure ()
       ensureSubmitted
     _ -> pure ()
